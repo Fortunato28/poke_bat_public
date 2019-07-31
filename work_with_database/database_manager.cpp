3badef4a40860d4742e3689cb165c38553002be9
Anton Sapunov <fort.sav.28@gmail.com>
2019-07-31 23:54:49 +0700

    Добавлен простейший коннект к базе

--- work_with_database/database_manager.cpp
+++ work_with_database/database_manager.cpp
@@ -1,4 +1,11 @@
 #include <iostream>
+#include <stdexcept>
+
+#include <mysql_connection.h>
+#include <mysql_driver.h>
+#include <cppconn/exception.h>
+#include <cppconn/resultset.h>
+#include <cppconn/statement.h>
 
 #include "database_manager.h"
 
@@ -8,7 +15,37 @@ namespace work_with_datbase {
 
 DBManager::DBManager()
 {
-    // TODO Connect to database
+    string url(EXAMPLE_HOST);
+    const string user(EXAMPLE_USER);
+    const string pass( EXAMPLE_PASS);
+    const string database(EXAMPLE_DB);
+
+    try
+    {
+        sql::Driver * driver = sql::mysql::get_driver_instance();
+        /* Using the Driver to create a connection */
+        std::unique_ptr< sql::Connection > con(driver->connect(url, user, pass));
+        con->setSchema(database);
+
+        std::unique_ptr< sql::Statement > stmt(con->createStatement());
+        std::unique_ptr< sql::ResultSet > res(stmt->executeQuery("SELECT 'Welcome to Connector/C++' AS _message"));
+        cout << "\t... running 'SELECT 'Welcome to Connector/C++' AS _message'" << endl;
+        while (res->next()) {
+          cout << "\t... MySQL replies: " << res->getString("_message") << endl;
+          cout << "\t... say it again, MySQL" << endl;
+          cout << "\t....MySQL replies: " << res->getString(1) << endl;
+        }
+
+    }
+    catch (sql::SQLException &e)
+    {
+        cout << "# ERR: SQLException in " << __FILE__;
+        cout << "(" << "EXAMPLE_FUNCTION" << ") on line " << __LINE__ << endl;
+        cout << "# ERR: " << e.what();
+        cout << " (MySQL error code: " << e.getErrorCode();
+        cout << ", SQLState: " << e.getSQLState() << " )" << endl;
+    }
+
 }
 
 DBManager::~DBManager()
--- work_with_database/database_manager.h
+++ work_with_database/database_manager.h
@@ -6,6 +6,12 @@ using lvl = std::size_t;
 
 namespace work_with_datbase {
 
+// TODO Think about it, mb it's bad
+#define EXAMPLE_HOST "localhost"
+#define EXAMPLE_USER "root"
+#define EXAMPLE_PASS "1"
+#define EXAMPLE_DB "test"
+
 class Pokemon{};
 
 class DBManager
@@ -13,12 +19,14 @@ class DBManager
 public:
     DBManager();
     ~DBManager();
-    void CreateDatabase();
 
     void AddPokemon(Pokemon& given_pok);
 
     Pokemon GetPokemon(lvl& level);
     void RemovePokemon();
 private:
+
+    // Тестовый метод, по факту при работе сервиса ничего создаваться не должно
+    void CreateDatabase();
 };
 } // namespace
