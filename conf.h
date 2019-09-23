3133052e59775d51a3b5d3ecb333e7d1f6bbb7ee
Anton Sapunov <fort.sav.28@gmail.com>
2019-09-24 00:28:34 +0700

    Изменены параметры базы - теперь подключается к докерному образу

--- conf.h
+++ conf.h
@@ -2,7 +2,7 @@
 #include <string>
 
 // TODO Think about it, mb it's bad
-const std::string host = "localhost";
+const std::string host = "172.17.0.2";
 const std::string user = "root";
 const std::string pass = "1";
 const std::string db = "poke_bat";
