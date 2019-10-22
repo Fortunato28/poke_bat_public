a3e258bf39b93ecac1a27b1da0182eb78c5b75f1
KeimaShikai <shikamaru740@gmail.com>
2019-10-22 15:21:16 +0700

    Fixed some missettings in checker and docker-compose

--- fhq-jury-ad-testing/jury.d/checkers/service2/checker.py
+++ fhq-jury-ad-testing/jury.d/checkers/service2/checker.py
@@ -134,10 +134,16 @@ def put_flag():
         print(s)
 
         transport.close()
-    except Exception as e:
-        traceback.print_exc()
+    except Thrift.TException as e:
         print(e)
-        raise e
+        service_corrupt()
+    except Thrift.TApplicationException as e:
+        print(e)
+        service_corrupt()
+    except Exception as e:
+        # traceback.print_exc()
+        print(e)
+        # raise e
         service_down()
 
 def check_flag():
@@ -157,6 +163,12 @@ def check_flag():
         print(flag2)
 
         transport.close()
+    except Thrift.TException as e:
+        print(e)
+        service_corrupt()
+    except Thrift.TApplicationException as e:
+        print(e)
+        service_corrupt()
     except Exception as e:
         print(e)
         service_down()
